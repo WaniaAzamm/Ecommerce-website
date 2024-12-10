a74c691f888aa471de0cd268abca9af03cd1badd
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-12-10 21:16:55 +0500

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -18,7 +18,7 @@ export default async function Hero() {
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
           <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
-            Where Time Meets Elegance!{" "}
+            Where Time Meets Elegance!!{" "}
           </h1>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
             Discover a world where precision meets prestige. Our collection of
